cfe26e21c568c505b97727f5816d9beb1a1f9376
Tonny Tzeng <tonny.tzeng@gmail.com>
2024-02-10 20:29:19 +0800

    evb4510: implement operators for icache/dcache commands.

--- arch/arm/mach-s3c4510b/cpu.c
+++ arch/arm/mach-s3c4510b/cpu.c
@@ -5,22 +5,67 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
-void enable_caches(void)
+void invalidate_icache_all(void)
 {
-    s32 i;
-    /* disable all cache bits */
-    clrbits(32, REG_SYSCFG, 0x3F);
-    /* 8KB cache, write enable */
-    setbits(32, REG_SYSCFG, CACHE_WRITE_BUFF | CACHE_MODE_01);
+    int i;
     /* clear TAG RAM bits */
     for (i = 0; i < 256; i++)
         out_32(CACHE_TAG_RAM + 4 * i, 0);
-    /* clear SET0 RAM */
-    for (i = 0; i < 1024; i++)
-        out_32(CACHE_SET0_RAM + 4 * i, 0);
-    /* clear SET1 RAM */
-    for (i = 0; i < 1024; i++)
-        out_32(CACHE_SET1_RAM + 4 * i, 0);
+}
+
+static int unified_cache_status(void)
+{
+    return (in_32(REG_SYSCFG) & CACHE_ENABLE);
+}
+
+static void unified_cache_enable(void)
+{
+    /* disable cache */
+    clrbits(32, REG_SYSCFG, CACHE_MODE | CACHE_ENABLE);
+    /* 8KB cache, write enable */
+    setbits(32, REG_SYSCFG, CACHE_WRITE_BUFF | CACHE_MODE_01);
+    invalidate_icache_all();
     /* enable cache */
     setbits(32, REG_SYSCFG, CACHE_ENABLE);
+}
+
+static void unified_cache_disable(void)
+{
+    /* disable all cache bits */
+    clrbits(32, REG_SYSCFG, CACHE_ENABLE);
+}
+
+int icache_status(void)
+{
+    return unified_cache_status();
+}
+
+void icache_enable(void)
+{
+    unified_cache_enable();
+}
+
+void icache_disable(void)
+{
+    unified_cache_disable();
+}
+
+int dcache_status(void)
+{
+    return unified_cache_status();
+}
+
+void dcache_enable(void)
+{
+    unified_cache_enable();
+}
+
+void dcache_disable(void)
+{
+    unified_cache_disable();
+}
+
+void enable_caches(void)
+{
+    unified_cache_enable();
 }
